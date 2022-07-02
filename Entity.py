aa051769749598ba9b213f993a9f2de108449d21
Amenhotep314 <nathan.lenzini@gmail.com>
2022-07-01 23:58:14 -0400

    Finishing basic implementation of Entity

--- Entity.py
+++ Entity.py
@@ -5,13 +5,18 @@ class Entity:
 
     """A generic interface implemented by all items in the game."""
 
-    def __init__(self, name, other_names, description, location, size=1, strength=0, capacity=0, hoistable=True, destroyable=False, open=False, openable=False, lightable=False, on=False):
+    def __init__(self,
+        name, other_names, description, location,
+        size=1, strength=0, capacity=0,
+        hoistable=True, destroyable=False, open=False, openable=False, lightable=False, on=False,
+        unlocks=None):
         
-        """Args:
+        """
+        Args:
             name (str): The primary title of the game entity
             other_names (tuple, str): Additional ways the entity may be referenced
             description (str): The detailed description of the entity
-            location (Room/Entity): Another object in the game which contains this instance
+            location (str): The name of another game object which contains this entity. Converted from str to object after initialization by snap
             size (int): The amount of space taken up by the entity (default is 1)
             strength (int): Resistance of entity to destruction or entity's destructive potential (default is 0)
             capacity (int): Units of size which can be contained in the entity (default is 0)
@@ -19,8 +24,9 @@ class Entity:
             destroyable (bool): Can the entity be destroyed? (default is False)
             open (bool): Is the entity open? (default is False)
             openable (bool): Can the entity be opened/closed? Is it unlocked? (default is False)
-            lightable (bool): Can the entity be used as a light? (default is False
-            on (bool): Is the entity on?"""
+            lightable (bool): Can the entity be used as a light? (default is False)
+            on (bool): Is the entity on? (default is False)
+            unlocks (tuple, str): The names of all entities which this entity can unlock. Converted from str to object after initialization by snap (default is None)"""
 
         self.names = (name,) + other_names
         self.description = description
@@ -34,6 +40,7 @@ class Entity:
         self.openable = openable
         self.lightable = lightable
         self.on = on
+        self.unlocks = unlocks
 
         if Game.DEBUG:
             Game.log("Entity initialized. Name: {name}.\tLocation: {location}".format(name=name, location=location))
@@ -42,17 +49,15 @@ class Entity:
     def __str__(self):
 
         """Overloads the builtin string method.
-        
         Returns:
             str: The primary name of the entity"""
         
-        return self.names[0]
+        return self.names[0].lower()
 
 
     def __repr__(self):
 
         """Overloads the builtin representation method.
-        
         Returns:
             str: The identifier of the entity for internal use"""
 
@@ -62,20 +67,24 @@ class Entity:
     def __eq__(self, other):
 
         """Overloads the builtin comparison method.
-
         Args:
-            other (Entity): Another entity
-        
+            other (Entity): Another entity  
         Returns:
             bool: Are the representations equal?"""
 
         return self.__repr__() == other.__repr__()
 
 
+    def snap(self):
+
+        """Converts strings to object references when possible. Should be called on all entities at session start."""
+
+        pass
+
+
     def contents(self):
 
         """Identifies entities which consider this entity their container.
-        
         Returns:
             tuple, Entity: All entities which this entity contains"""
 
@@ -91,7 +100,6 @@ class Entity:
     def contents_size(self):
 
         """Sums the size attributes of each entity which considers this entity its container.
-        
         Returns:
             int: The size of this entity's contents"""
 
@@ -113,7 +121,6 @@ class Entity:
     def examine(self):
 
         """Gives further details about the entity when the player requests them.
-        
         Returns:
             str: Description of entity"""
 
@@ -123,7 +130,6 @@ class Entity:
     def take(self):
 
         """Puts the entity in the player's inventory if possible.
-        
         Returns:
             str: Success/failure message"""
 
@@ -139,7 +145,6 @@ class Entity:
     def drop(self):
 
         """Puts the entity in the player's current location.
-        
         Returns:
             str: Success message"""
 
@@ -147,10 +152,26 @@ class Entity:
         return "Dropped."
 
 
+    def put_in(self, other):
+
+        """Puts the entity in the target location if possible.
+        Args:
+            other (Entity): The object which the entity will be put into    
+        Returns:
+            str: Success/failure message"""
+
+        if not other.openable:
+            return "The {other_name} is not the sort of thing you can put the {name} into.".format(other_name=str(other), name=str(self))
+        elif not other.open:
+            return "The {other_name} is closed.".format(other_name=str(other))
+        else:
+            self.location = other
+            return "The {name} is now in the {other_name}.".format(name=str(self), other_name=str(other))
+
+
     def open(self):
 
         """Opens the entity if possible.
-        
         Returns:
             str: Success/failure message"""
 
@@ -166,7 +187,6 @@ class Entity:
     def close(self):
 
         """Closes the entity if possible.
-        
         Returns:
             str: Success/failure message"""
 
@@ -179,10 +199,43 @@ class Entity:
             return "Closed."
 
 
+    def unlock(self, unlocker):
+
+        """Allows the entity to be opened if possible.
+        Args:
+            unlocker (Entity): The item being used to unlock this entity    
+        Returns:
+            str: Success/failure message"""
+
+        if self.openable:
+            return "The {name} can already be opened."
+        elif self in unlocker.unlocks:
+            self.openable = True
+            return "You unlock the {name}.".format(name=str(self))
+        else:
+            return "You cannot unlock the {name} with the {other_name}.".format(name=str(self), other_name=str(unlocker))
+
+    
+    def lock(self, locker):
+
+        """Prevents the entity from being opened if possible.
+        Args:
+            locker (Entity): The item being used to lock this entity     
+        Returns:
+            str: Success/failure message"""
+
+        if not self.openable:
+            return "The {name} is already unopenable.".format(name=str(self))
+        elif self in locker.unlocks:
+            self.openable = False
+            return "You lock the {name}".format(name=str(self))
+        else:
+            return "You cannot lock the {name} with the {other_name}".format(name=str(self), other_name=str(locker))
+
+
     def turn_on(self):
 
         """Turns on the entity if possible.
-        
         Returns:
             str: Successs/failure message"""
 
@@ -198,21 +251,22 @@ class Entity:
     def turn_off(self):
 
         """Turns off the entity if possible.
-        
         Returns:
             str: Successs/failure message"""
 
         if not self.lightable:
             return "The {name} is not a thing you can turn off.".format(name=str(self))
+        elif not self.on:
+            return "The {name} is already on.".format(name=str(self))
+        else:
+            return "It is now off."
 
 
     def hit(self, indirect_object):
 
         """Handles this entity being hit with another entity.
-        
         Args:
-            indirect_object (Entity): The entity being used to hit this one
-            
+            indirect_object (Entity): The entity being used to hit this one 
         Returns:
             str: Success/failure message"""
 
@@ -227,4 +281,12 @@ class Entity:
                 return "You damage the {other_name}.".format(other_name=str(indirect_object))
 
 
-    
+    def say(self, text):
+
+        """Handles this entity being spoken to.
+        Args:
+            text (str): The words said to the entity    
+        Returns:
+            str: The entity's response or a failure message"""
+
+        return "The {name} is unresponsive.".format(name=str(self))
