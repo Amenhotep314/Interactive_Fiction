60ddda136c11ba437bbe8df41378c40ec2baa1ac
Amenhotep314 <nathan.lenzini@gmail.com>
2022-07-01 23:58:30 -0400

    Tweaking main game functions

--- Game.py
+++ Game.py
@@ -1,21 +1,39 @@
 DEBUG = True
-
+turn = 0
 
 def main():
 
     """Executed from the command line and calls all other parts of the game."""
     
     if DEBUG:
-        log("NEW SESSION STARTED.")
+        log("Session started.")
+    
+    # Import all entities and rooms
+    # Snap all entities and rooms
+
+    from Entity import Entity
+    test = Entity(
+        "Brass Lantern",
+        ("lantern", "lamp", "light"),
+        "The brass lantern is highly hoistable.",
+        "Attic"
+    )
+
+    while True:
+        turn_handler(1)
+        # Background tasks
+        # Display update
+        # User input
+        # Parse
+        # Turn tasks
+        break
 
 
 def turn_handler(increment=0):
     
     """Changes and/or accesses the number of the current turn.
-    
     Args:
         increment (int): The amount by which to change the turn (default is 0)
-    
     Returns:
         int: The current turn"""
     
@@ -27,7 +45,6 @@ def turn_handler(increment=0):
 def log(event):
     
     """Writes the given event to log.txt, along with the date, time, and turn.
-
     Args:
         event (str): The text to be logged"""
 
@@ -36,10 +53,9 @@ def log(event):
     time = time.strftime("%Y/%m/%d - %H:%M:%S - Turn " + str(turn))
 
     with open("log.txt", "a") as log:
-        log.write(time + ": " + event + "\n")
+        log.write(time + ": " + str(event) + "\n")
 
 
 if __name__ == "__main__":
     
-    turn = 0
     main()
