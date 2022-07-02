15455942b68d3db05815e1f2a3101f7c6c2a4995
MoneyBags-XIV <isaac.lenzini@gmail.com>
2022-07-02 19:59:46 -0400

    Adds main parse function with description and imports

--- Parser.py
+++ Parser.py
@@ -1,1 +1,12 @@
-#It was just a mispelling, Nathan.
+import Game
+
+def parse(userInput):
+
+    """This is the main function for the parser.
+    Pass the input from the user in as a string, and it will call the requisite method in whatever entity.
+    It does not return anything, but it does write tot the log."""
+
+    if Game.DEBUG:
+        Game.log("Parser received phrase: " + userInput)
+
+    
