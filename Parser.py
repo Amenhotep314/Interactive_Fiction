739622e6ef9c904c670d16a03640d99fae950d61
MoneyBags-XIV <isaac.lenzini@gmail.com>
2022-07-02 20:01:48 -0400

    Fixes misspelling in description

--- Parser.py
+++ Parser.py
@@ -4,7 +4,7 @@ def parse(userInput):
 
     """This is the main function for the parser.
     Pass the input from the user in as a string, and it will call the requisite method in whatever entity.
-    It does not return anything, but it does write tot the log."""
+    It does not return anything, but it does write to the log."""
 
     if Game.DEBUG:
         Game.log("Parser received phrase: " + userInput)
