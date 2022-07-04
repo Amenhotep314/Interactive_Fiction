922a1b266066590051796726dee45d5037c4c8cc
MoneyBags-XIV <isaac.lenzini@gmail.com>
2022-07-04 10:56:41 -0400

    adds call to cleanInput funcion

--- Parser.py
+++ Parser.py
@@ -4,7 +4,8 @@ def parse(userInput):
 
     """This is the main function for the parser.
     Pass the input from the user in as a string, and it will call the requisite method in whatever entity.
-    It does not return anything, but it does write to the log."""
+
+    cleanText = cleanInput(userInput)
 
     if Game.DEBUG:
     verbs = (
