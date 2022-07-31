26feafc37780a8dcb9fcc55dcebd4be9e7ec1970
MoneyBags-XIV <isaac.lenzini@gmail.com>
2022-07-31 12:56:55 -0400

    You know what? I'm tired of making tiny commits. This is a giant one.

--- Parser.py
+++ Parser.py
@@ -1,6 +1,4 @@
-import Game
-import Entity
-import Room
+#from Game import game
 
 def parse(user_input):
 
@@ -10,14 +8,55 @@ def parse(user_input):
 
     user_input_arts = clean_input(user_input)
     user_input = remove_arcticles(user_input_arts)
-    
-    verb = find_verb(user_input)
+    unknown = check_unknown_words(user_input)
 
-    print(verb)
+    if unknown:
+        print("I don't know the word: \"" + unknown + ".\"")
 
-    if verb:
+    else:
+        verb, og_verb = find_verb(user_input)
 
-        needs_direct = requires_object(verb, "direct")
+        if verb:
+            needs_direct = requires_object(verb, "direct")
+
+            if needs_direct:
+                direct = find_direct_object(user_input, og_verb)
+
+                if direct:
+                    if len(direct) > 1 and not supports_multiple_direct_objects(verb):
+                        print("You can't use more than one direct object with the verb: \"" + og_verb + ".\"")
+
+                    else:
+                        needs_indirect = requires_object(verb, "indirect")
+
+                        if needs_indirect:
+                            indirect = find_indirect_object(user_input, og_verb, direct[0])
+
+                            if len(indirect) > 1:
+                                print("You can't use multiple indirect objects with the verb: \"" + og_verb + ".\"")
+
+                            elif len(indirect) == 1:
+                                execute(verb, direct, indirect)
+
+                        else:
+                            execute(verb, direct)
+
+            else:
+                execute(verb, "player")
+
+
+def supports_multiple_direct_objects(verb):
+
+    supports_list = ("take", "drop")
+
+    if verb in supports_list:
+
+        return True
+
+    else:
+
+        return False
+
 
 def requires_object(verb, object_type):
 
@@ -25,7 +64,7 @@ def requires_object(verb, object_type):
     Args:
         verb (str): the verb in question
         objectType (str): either 'direct' or 'indirect'"""
-    
+
     requires_object_list = {
         "n": (False, False),
         "s": (False, False),
@@ -66,10 +105,10 @@ def requires_object(verb, object_type):
     elif object_type == "indirect":
 
         requires = requires_object_list[verb][1]
-    
+
     return requires
 
-    
+
 def find_verb(text):
 
     """Finds the verb or verbs in a string of text.
@@ -111,13 +150,14 @@ def find_verb(text):
     ]
 
     verb = ""
+    og_verb = ""
     verb_count = 0
     word_list = text.split(" ")
 
     if text == "i":
         verb = "inventory"
         verb_count += 1
-    
+
     else:
 
         for i in range(len(verbs)):
@@ -145,24 +185,24 @@ def find_verb(text):
 
                                     verb = verbs[i][j]
                                     verb_count += 1
-                            
+
                             else:
 
                                 verb = verbs[i][j]
                                 verb_count += 1
-                        
+
                         else:
 
                             verb = verbs[i][j]
                             verb_count += 1
-                    
+
                     if a+1 != len(word_list):
 
                         if word_list[a] + " " + word_list[a+1] == verbs[i][j]:
 
                             verb = word_list[a] + " " + word_list[a+1]
                             verb_count += 1
-    
+
     directions = (
             "n",
             "s",
@@ -209,15 +249,15 @@ def find_verb(text):
 
                     direction = True
                     direction_index = i
-        
+
         if direction == True:
 
             verb = directions[direction_index]
-        
+
         else:
             print("That's not a direction you can go!")
             return ""
-            
+
     if verb_count == 1:
 
         for i in range(len(verbs)):
@@ -227,27 +267,28 @@ def find_verb(text):
                 if verb == verbs[i][j]:
 
                     verb = verbs[i][0]
-        
-        return verb
+                    og_verb = verbs[i][j]
+
+        return verb, og_verb
 
     elif verb_count == 0:
 
         print("There's no verb in that sentence!")
-        return ""
-    
+        return "", ""
+
     elif verb_count > 1:
 
         print("I don't understand that sentence!")
-        return ""
+        return "", ""
 
 
 def execute(verb, direct, indirect="", str_indirect=False):
-    
-    direct_object = Game.game.object_from_str(direct)
+
+    direct_object = game.object_from_str(direct)
 
     if indirect and not str_indirect:
 
-        indirect_object = Game.game.object_from_str(indirect)
+        indirect_object = game.object_from_str(indirect)
 
     method = getattr(direct_object, verb)
     output = method(indirect_object)
@@ -256,12 +297,11 @@ def execute(verb, direct, indirect="", str_indirect=False):
 
 def check_unknown_words(text):
 
-    """This is NOT done yet. Don't even try to test it. It's no good."""
-
     word_list = text.split(" ")
+    known_list = [False for x in word_list]
     unknown_word = ""
 
-    white_list = ("and", "to", "all", "use", "using", "with")
+    white_list = ("and", "to", "all", "use", "using", "with", "i")
 
     verbs = [
         "move", "go",
@@ -295,14 +335,137 @@ def check_unknown_words(text):
         "drink", "guzzle", "sip", "swallow", "swig", "slurp"
     ]
 
-    for word in word_list:
+    for i in range(len(word_list)-1):
 
-        if word not in verbs and not Game.game.object_from_str(word):
+        if word_list[i] + " " + word_list[i+1] in verbs or game.object_from_str(word_list[i] + " " + word_list[i+1]) or word_list[i] + " " + word_list[i+1] in white_list:
 
-            unknown_word = word
+            known_list[i] = True
+            known_list[i+1] = True
+
+    for i in range(len(word_list)):
+
+        if word_list[i] in verbs or game.object_from_str(word_list[i]) or word_list[i] in white_list:
+
+            known_list[i] = True
+
+    for i in range(len(word_list)):
+
+        if not known_list[i]:
+
+            unknown_word = word_list[i]
             break
-    
+
     return unknown_word
+
+
+def find_indirect_object(text, verb, direct):
+
+    word_list = text.split()
+    verb_words = verb.split()
+
+    if len(verb_words) > 1:
+
+        for i in range(len(word_list)):
+
+            if word_list[i] == verb_words[0]:
+
+                word_list[i:i+(len(verb_words)-1)] = verb
+                break
+
+
+    indirect_object = []
+
+    verb_index = word_list.index(verb)
+
+    indirect_qualifyers = ("use", "using", "with")
+    qualifyer_index = None
+    qualifyer_count = 0
+
+    for i in range(len(word_list)):
+
+        for qualifyer in indirect_qualifyers:
+
+            if word_list[i] == qualifyer:
+
+                qualifyer_index = i
+                qualifyer_count += 1
+
+    if qualifyer_count > 1:
+
+        print("I don't understand that sentence!")
+
+    if qualifyer_count <= 1:
+
+        if qualifyer_count == 1:
+
+            if qualifyer_index < verb_index:
+
+                word_list = word_list[:verb_index]
+
+            elif qualifyer_index > verb_index:
+
+                word_list = word_list[qualifyer_index:]
+
+            counted_list = [False for x in word_list]
+
+            for i in range(len(word_list)-1):
+
+                if game.object_from_str(word_list[i] + " " + word_list[i+1]) and not counted_list[i] and not counted_list[i+1]:
+
+                    indirect_object.append(word_list[i] + " " + word_list[i+1])
+                    counted_list[i] = True
+                    counted_list[i+1] = True
+
+            for i in range(len(word_list)):
+
+                if game.object_from_str(word_list[i]) and not counted_list[i]:
+
+                    indirect_object.append(word_list[i])
+                    counted_list[i] = True
+
+        if not indirect_object:
+
+            user_input = input("What do you want to " + verb + " the " + direct + " with?\n>>> ")
+            user_input = clean_input(user_input)
+            user_input = remove_arcticles(user_input)
+            unknown = check_unknown_words(user_input)
+
+            if unknown:
+
+                print("I don't know the word: \"" + unknown +".\"")
+
+            else:
+
+                input_words = user_input.split()
+                counted_list = [False for x in input_words]
+
+                for i in range(len(input_words)-1):
+
+                    if game.object_from_str(input_words[i] + " " + input_words[i+1]) and not counted_list[i] and not counted_list[i+1]:
+
+                        indirect_object.append(input_words[i] + " " + input_words[i+1])
+                        counted_list[i] = True
+                        counted_list[i+1] = True
+
+                for i in range(len(input_words)):
+
+                    if game.object_from_str(input_words[i]) and not counted_list[i]:
+
+                        indirect_object.append(input_words[i])
+                        counted_list[i] = True
+
+                    elif input_words[i] == "all":
+
+                        indirect_object.append(input_words[i])
+                        counted_list[i] = True
+
+                if not indirect_object:
+
+                    print("What?")
+
+    return indirect_object
+
+
 
 
 def find_direct_object(text, verb):
@@ -316,9 +479,10 @@ def find_direct_object(text, verb):
 
             if word_list[i] == verb_words[0]:
 
-                word_list[i:i+(len(verb_words)-1)] = verb
+                word_list[i] = verb
+                del word_list[i+1]
                 break
-                
+
 
     direct_object = []
 
@@ -342,11 +506,11 @@ def find_direct_object(text, verb):
         if qualifyer_index < verb_index:
 
             word_list = word_list[verb_index:]
-        
+
         elif qualifyer_index > verb_index:
 
             word_list = word_list[:qualifyer_index]
-    
+
     if qualifyer_count > 1:
 
         print("I don't understand that sentence!")
@@ -360,7 +524,7 @@ def find_direct_object(text, verb):
 
         for i in range(len(word_list)-1):
 
-            if Game.game.object_from_str(word_list[i] + " " + word_list[i+1]) and not counted_list[i] and not counted_list[i+1]:
+            if game.object_from_str(word_list[i] + " " + word_list[i+1]) and not counted_list[i] and not counted_list[i+1]:
 
                 direct_object.append(word_list[i] + " " + word_list[i+1])
                 counted_list[i] = True
@@ -368,14 +532,19 @@ def find_direct_object(text, verb):
 
         for i in range(len(word_list)):
 
-            if Game.game.object_from_str(word_list[i]) and not counted_list[i]:
+            if game.object_from_str(word_list[i]) and not counted_list[i]:
 
                 direct_object.append(word_list[i])
                 counted_list[i] = True
-        
+
+            elif word_list[i] in ("all", "everything",):
+
+                direct_object.append("all")
+                counted_list[i] = True
+
         if not direct_object:
 
-            user_input = input("What do you want to " + verb + "?")
+            user_input = input("What do you want to " + verb + "?\n>>> ")
             user_input = clean_input(user_input)
             user_input = remove_arcticles(user_input)
             unknown = check_unknown_words(user_input)
@@ -383,7 +552,7 @@ def find_direct_object(text, verb):
             if unknown:
 
                 print("I don't know the word: \"" + unknown +".\"")
-            
+
             else:
 
                 input_words = user_input.split()
@@ -391,7 +560,7 @@ def find_direct_object(text, verb):
 
                 for i in range(len(input_words)-1):
 
-                    if Game.game.object_from_str(input_words[i] + " " + input_words[i+1]) and not counted_list[i] and not counted_list[i+1]:
+                    if game.object_from_str(input_words[i] + " " + input_words[i+1]) and not counted_list[i] and not counted_list[i+1]:
 
                         direct_object.append(input_words[i] + " " + input_words[i+1])
                         counted_list[i] = True
@@ -399,17 +568,22 @@ def find_direct_object(text, verb):
 
                 for i in range(len(input_words)):
 
-                    if Game.game.object_from_str(input_words[i]) and not counted_list[i]:
+                    if game.object_from_str(input_words[i]) and not counted_list[i]:
 
                         direct_object.append(input_words[i])
                         counted_list[i] = True
-                
+
+                    elif input_words[i] == "all":
+
+                        direct_object.append(input_words[i])
+                        counted_list[i] = True
+
                 if not direct_object:
 
                     print("What?")
-    
+
     return direct_object
-                
+
 
 def remove_arcticles(user_input):
 
@@ -421,12 +595,18 @@ def remove_arcticles(user_input):
 
     article_blacklist = ("a", "an", "the", "this", "that")
 
-    for i in range(len(article_blacklist)):
+    word_list = user_input.split()
 
-        user_input = user_input.replace(article_blacklist[i]+" ", "")
-        user_input = user_input.replace(" "+article_blacklist[i], "")
-    
-    return user_input
+    for article in article_blacklist:
+
+        if article in word_list:
+
+            for i in range(word_list.count(article)):
+
+                word_list.remove(article)
+
+    return " ".join(word_list)
+
 
 def clean_input(user_input):
 
@@ -435,7 +615,7 @@ def clean_input(user_input):
         userInput (str): the string to have its punctuation removed
     Returns:
         str: string without puctuation or caps"""
-    
+
     punc_blacklist = (".", ",", "<", ">", "?", "/", "\\", ";", ":", "'", "[", "]", "{", "}", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "`", "~")
 
     for i in range(len(punc_blacklist)):
@@ -445,6 +625,3 @@ def clean_input(user_input):
     user_input = user_input.lower()
 
     return user_input
-
-def move(direction):
-    Game.game.player.move(direction)
